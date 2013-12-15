ae933f5c5419ea7a8c82ac7c70ab1545f049c71a
Julian Birch <julian.birch@gmail.com>
2013-12-15 20:29:37 +0000

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject net.colourcoding/ulisse "0.1.1"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.python/jython-standalone "2.5.3"]
+                 [org.pygments/pygments "1.5"]
+                 [net.colourcoding/poppea "0.1.7"]
+                 [spyscope "0.1.3"]
+                 [codox-md "0.2.0"]
+                 [endophile "0.1.2"]
+                 [codox/codox.core "0.6.4"]])
